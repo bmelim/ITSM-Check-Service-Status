2b90559afa950c0b89e177f1c1eca6ba3e93da1b
elvisangel <elvis.angel@gmail.com>
2019-04-02 08:47:32 +0200

    Update CheckServiceStatus.php

--- CheckServiceStatus.php
+++ CheckServiceStatus.php
@@ -1,18 +1,17 @@
 class CheckServiceStatus {
 
 
-	// insert test ticket here, class will check number and status of the ticket
-	// edit only in this var section, no need to edit function
+	
 	var $testticket = 'SR17252689';
 	var $desiredstatus = 'Closed';
-	var $wsdl = 'https://jciebonding.jci.com:7843/SCSMIntegrationService.svc?singleWsdl';
+	var $wsdl = 'ebonding site';
 	
 	function checkStatus() {
 	
 		
 		$ticketinfo = array('strWorkItemType' => 'Service Request', 
                       'strID' => $this->testticket,   
-                      'strGlobalID' => '',      // working only in QA
+                            
                       'strStatus' => '', 
                       'strTitle'=> '', 
                       'strDesc' => '', 
